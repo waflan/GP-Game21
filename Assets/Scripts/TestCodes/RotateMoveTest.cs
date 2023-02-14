d793a53a4eec4981efa163a8941514889e041271
wafy <waflan@users.noreply.github.com>
2023-02-14 18:18:09 +0900

    デバッグ２

--- Assets/Scripts/TestCodes/RotateMoveTest.cs
+++ Assets/Scripts/TestCodes/RotateMoveTest.cs
@@ -6,6 +6,8 @@ public class RotateMoveTest : MonoBehaviour
 {
     KeyConfig keyConfig;
 
+    public AnimationCurve curve;
+
     public float speed = 10;
     public Vector2 camDistanceRange=new Vector2(1,10);
     public Vector2 rotateSpeed=new Vector2(270,180); // カメラ回転速度
@@ -90,18 +92,26 @@ public class RotateMoveTest : MonoBehaviour
             // Quaternion rot = Quaternion.AngleAxis(rx,Vector3.up);
             // Debug.Log(Mathf.DeltaAngle(rx,Mathf.Atan2(rot.w,rot.y)*Mathf.Rad2Deg));
             Vector3 axis;
-            float angle;
+            float angle,deltaHolAngle;
+            deltaHolAngle = (Mathf.Rad2Deg*Mathf.Atan2(playerUpVector.x,playerUpVector.z)-Mathf.Rad2Deg*Mathf.Atan2(beforeUpVector.x,beforeUpVector.z));
+            deltaHolAngle = Mathf.Repeat(deltaHolAngle+180,360)-180;
 
             Quaternion.FromToRotation(beforeUpVector,playerUpVector).ToAngleAxis(out angle,out axis);
             Debug.DrawRay(RotateCenter.position,axis,Color.blue);
-            angle = Vector3.Angle(Vector3.up,axis)-90;
+            angle = (90 - Mathf.Abs(Vector3.Angle(Vector3.up,axis)-90))*Mathf.Deg2Rad;
 
-
-            
+            // Debug.Log(angle);
+            Debug.Log(deltaHolAngle);
             // Debug.Log($"{Mathf.Atan2(nextLocalRot.y,nextLocalRot.w)*2*Mathf.Rad2Deg:F2}:({angle:F2},{axis})");
 
             if(Input.GetKey(KeyCode.LeftShift)){
-                rx+=(Mathf.Rad2Deg*Mathf.Atan2(playerUpVector.x,playerUpVector.z)-Mathf.Rad2Deg*Mathf.Atan2(beforeUpVector.x,beforeUpVector.z))*(angle/90);
+                float hoge = angle/Mathf.PI*2;
+                hoge=curve.Evaluate(hoge);
+                if(playerUpVector.y>0){
+                    rx-=(deltaHolAngle)*(hoge);
+                }else{
+                    rx+=(deltaHolAngle)*(hoge);
+                }
                 // nextLocalRot.ToAngleAxis(out angle,out axis);
                 // rx = (axis.y>0?1:-1)*angle;
             }else{
